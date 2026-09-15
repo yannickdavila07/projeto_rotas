4f0d62d5aee945a4e88758d4098635b9e64f3076
Yannick <davilayannick07@gmail.com>
2026-09-15 10:58:01 -0300

    feat:Instalando dependências de rotas e importando no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import NavBar from "./components/NavBar"
+import Home from "./pages/Home"
+import Sobre from "./pages/Contato"
+import Produtos from "./pages/Produtos"
+import Contato from "./pages/Contato"
 
 const App = () => {
   return (
